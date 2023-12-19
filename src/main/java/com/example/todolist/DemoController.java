9391f3d5640b8f838c77c39f9004f4305e3c12b1
SakaiIsao <sakai-isao@mgt-net.com>
2023-12-19 16:02:23 +0900

    index.htmlを返すようControllerクラスを修正

--- src/main/java/com/example/todolist/DemoController.java
+++ src/main/java/com/example/todolist/DemoController.java
@@ -1,13 +1,14 @@
 package com.example.todolist;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //このクラスがWEBアプリケーションとして操作できるコントローラプログラムとして動作できるようになります。
+@Controller //このクラスがWEBアプリケーションとして操作できるコントローラプログラムとして動作できるようになります。
 public class DemoController {
 	
 	@RequestMapping("/") //http://localhost:8080/でアクセスされた場合
 	String Home() {
-		return "Hello World2";
+		return "index";		//resources/index.html
 	}
 }
