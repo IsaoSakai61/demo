d38998ceae0f9dd8cbfdc6c1b6e485a7a4880bc9
SakaiIsao <sakai-isao@mgt-net.com>
2023-12-19 16:12:47 +0900

    コントローラーとビューを連携するよう修正 localhost:8080/numでnumまでの加算値を表示する

--- src/main/java/com/example/todolist/DemoController.java
+++ src/main/java/com/example/todolist/DemoController.java
@@ -1,14 +1,21 @@
 package com.example.todolist;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller //このクラスがWEBアプリケーションとして操作できるコントローラプログラムとして動作できるようになります。
 public class DemoController {
 	
-	@RequestMapping("/") //http://localhost:8080/でアクセスされた場合
-	String Home() {
+	@RequestMapping("/{num}") //http://localhost:8080/numでアクセスされた場合
+	public String index(@PathVariable int num, Model model) {	//ModelはWebページで使用するデータを管理するクラスです。
+		int sum = 0;
+		for(int i=0; i<=num; i++) {
+			sum += i;
+		}
+		model.addAttribute("msg","sum=" + sum);//ビュー側で第一引数で指定した名前でデータを取り出します。
 		return "index";		//resources/index.html
 	}
 }
